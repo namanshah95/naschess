4b14aeee6d30b735ecf652d5eda9ead5d2aa5ab3
namanshah95 <namanshah66@gmail.com>
2017-06-07 07:48:39 -0400

    More admin-specific authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     return new_user_session_url unless user_signed_in?
-    user_url
+    profile
   end
 
   protected
